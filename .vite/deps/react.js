3c0a9e0b7e0ad06e1a2a4837f958e13eabfb3543
SALİM SERHAT AKA <salimaka2014@gmail.com>
2025-07-10 02:22:14 +0300

    done

--- .vite/deps/react.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_react
-} from "./chunk-D2KY6O3O.js";
-import "./chunk-V4OQ3NZ2.js";
-export default require_react();
--- backend/routes/messageRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const messageController = require('../controllers/messageController');
-const auth = require('../middlewares/authMiddleware');
-
-router.post('/', messageController.createMessage);
-router.get('/', auth, messageController.getMessages);
-
-module.exports = router;
