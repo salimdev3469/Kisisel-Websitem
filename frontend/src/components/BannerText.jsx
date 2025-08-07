94a632c3f1253d277b80f75f16e8460fcdcf669f
Salim Serhat AKA <157348323+salimdev3469@users.noreply.github.com>
2025-08-07 18:06:24 +0300

    Update BannerText.jsx

--- frontend/src/components/BannerText.jsx
+++ frontend/src/components/BannerText.jsx
@@ -8,17 +8,19 @@ function BannerText() {
             <p>Full-Stack Yazılım Geliştirici</p>
             <TypeAnimation
                 sequence={[
-                    'Web ve Mobil Sistemler için iletişime geçin.',
+                    'Web teknolojileri için iletişime geçin.',
                     900,
-                    'Dağıtık Sistemler için iletişime geçin.',
+                    'Mobil Sistemler için iletişime geçin.',
                     900,
                     'Bulut Sistemler için iletişime geçin.',
+                    900,
+                    'CRM/ERP çözümleri için iletişime geçin.',
                     900,
                 ]}
                 id='slogan'
                 wrapper="span"
-                speed={60}
-                style={{ fontSize: '1.5em', display: 'inline-block', fontFamily: "Inter", }}
+                speed={70}
+                style={{ fontSize: '1.8em', display: 'inline-block', fontFamily: "Inter", }}
                 repeat={Infinity}
             />
         </div>
